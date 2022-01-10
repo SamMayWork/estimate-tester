e21381e6caea8828eddcfdf415a3df0de885dbba
Samuel May <sam.may.2412@gmail.com>
2022-01-10 11:28:03 +0000

    Changes

--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -f ./docker/Dockerfile --tag ssmay0/chiron-template .
--- src/app.js
+++ src/app.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
+const data = []
+
+const printRequest = (req, res, next) => {
+  console.log(`Request for ${req.method}`)
+  next()
+}
+
+app.use(printRequest)
+
 app.get('/', (req, res) => {
   res.send('OK')
+})
+
+app.post('/api/data', (req, res) => {
+  data.push(req.body)
+  res.sendStatus(200)
+})
+
+app.all('*', (req, res) => {
+  res.sendStatus(404)
 })
 
 module.exports = app
